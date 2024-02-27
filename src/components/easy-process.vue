dc6a15471702b38d5b8161e3e1816de889ce3f4e
ahmadhasnain0 <ahmadhasnain4554@gmail.com>
2024-02-27 22:54:55 +0500

    easy-process

--- src/components/easy-process.vue
+++ src/components/easy-process.vue
@@ -9,7 +9,52 @@
         We specialize in helping you build a team of expert<br> developers, testers, and leaders.
     </p>
 </div>
+<div class="col-6 col-md-3 mt-3 justify-content-center align-items-center tex-center "
+>
+<div class="w-100 d-flex justify-content-center align-items-center">
+    <img src="../assets/images/easy-process/Group.png" width="135px" height="135px" alt="group pic" class="object-fit-contain">
 
+</div>
+<div class="w-100 d-flex justify-content-center align-items-center mt-3">
+    <p class="text-blue text-uppercase fw-bold f-18 ">
+<span class="h2">01 </span>  you ask  </p>
+</div>
+
+</div>
+<div class="col-6 col-md-3 mt-3 justify-content-center align-items-center tex-center "
+>
+<div class="w-100 d-flex justify-content-center align-items-center">
+    <img src="../assets/images/easy-process/settings.png" width="135px" height="135px" alt="group pic" class="object-fit-contain">
+
+</div>
+<div class="w-100 d-flex justify-content-center align-items-center mt-3">
+    <p class="text-blue text-uppercase fw-bold f-18 ">
+<span class="h2">02 </span>  you proceed  </p>
+</div>
+
+</div><div class="col-6 col-md-3 mt-3 justify-content-center align-items-center tex-center "
+>
+<div class="w-100 d-flex justify-content-center align-items-center">
+    <img src="../assets/images/easy-process/handshake 3.png" width="135px" height="135px" alt="group pic" class="object-fit-contain">
+
+</div>
+<div class="w-100 d-flex justify-content-center align-items-center mt-3">
+    <p class="text-blue text-uppercase fw-bold f-18 ">
+<span class="h2">03 </span>  you Negotiate  </p>
+</div>
+
+</div><div class="col-6 col-md-3 mt-3 justify-content-center align-items-center tex-center "
+>
+<div class="w-100 d-flex justify-content-center align-items-center">
+    <img src="../assets/images/easy-process/medal 3.png" width="135px" height="135px" alt="group pic" class="object-fit-contain">
+
+</div>
+<div class="w-100 d-flex justify-content-center align-items-center mt-3">
+    <p class="text-blue text-uppercase fw-bold f-18 ">
+<span class="h2">04 </span>  you get  </p>
+</div>
+
+</div>
       </div>
       </div>
       </div>
