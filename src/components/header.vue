d23ef87d27f05380865bef374d957addd74c4b1c
ahmadhasnain0 <ahmadhasnain4554@gmail.com>
2024-02-27 18:14:38 +0500

    header

---
+++ src/components/header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container-fluid white pb-5">
+    <div class="container px-3 px-sm-0 w-100 align-items-center">
+      <div class="row">
+
+        <div class="col-12 col-sm-4 pt-auto pt-sm-5">
+             <div class="card border-0 bg-white ">
+            <div class="card-body  px-0 py-0 py-sm-3 position-absolute bg-white card-header border-0">
+         
+              
+                  <h1 class="fw-bolder text-uppercase text-blue  mt-3 pe-0 pe-sm-2">
+                    Building dev team, Quickly and Affordably                  </h1>
+                
+                  <div class="py-3 text-black">
+                    We specialize in helping you build a team of expert developers, testers, and leaders. We are setup to deliver long term solutions, or scale to different needs quickly.
+              </div>
+              <button class="btn bg-primary px-3 text-white mt-3 mb-4">
+    Build a team <img src="../assets/images/header/arrow_forward.png">
+</button>
+            </div>
+          </div></div>
+        <div class="col-12 col-sm-8 d-none d-sm-inline">
+            <img src="../assets/images/header/header-pic.png" width="100%" height="90%" class="object-fit-stretech">
+        </div>
+
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+
+import "@/src/assets/scss/components/_header.scss";
+</script>
--- src/pages/landing-page.vue
+++ src/pages/landing-page.vue
@@ -1,9 +1,11 @@
 <template>
    <Nav />
+   <Header />
 </template>
 
 <script setup>
 import Nav from "../components/nav.vue"
+import Header from "../components/header.vue"
 
 
 </script>
