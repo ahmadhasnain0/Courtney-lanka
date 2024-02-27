2417b3d091d594b0aa1157061602724f8741cf7a
ahmadhasnain0 <ahmadhasnain4554@gmail.com>
2024-02-28 00:07:46 +0500

    specialized

---
+++ src/components/specialized.vue
@@ -0,0 +1,237 @@
+<template>
+  <div class="container-fluid text-blue  bg-white">
+    <div class="container px-3 px-sm-0 w-100 align-items-center py-5">
+      <div class="row mt-4">
+        <div class="col-12 col-md-7 text-start">
+          <p class="f-14 fw-bold text-blue mb-2">Specialized</p>
+          <h1 class="fw-bold">Specialized Staff We Provide</h1>
+        </div>
+        <div class="col-5 d-none d-md-inline"></div>
+        <div class="col-12 col-sm-6 col-lg-3 mt-3">
+          <div class="card card-specialized rounded-3">
+            <div class="card-body d-flex align-items-center gap-3">
+              <img
+                src="../assets/images/specialized/frontend 2.png"
+                width="52px"
+                height="52px"
+                alt="frontend"
+              />
+              <h6 class="text-blue">Frontend Engineers</h6>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-sm-6 col-lg-3 mt-3">
+          <div class="card card-specialized rounded-3">
+            <div class="card-body d-flex align-items-center gap-3">
+              <img
+                src="../assets/images/specialized/angular_solidBlack.png"
+                width="52px"
+                height="52px"
+                alt="frontend"
+              />
+              <h6 class="text-blue">Angular Developers</h6>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-sm-6 col-lg-3 mt-3">
+          <div class="card card-specialized rounded-3">
+            <div class="card-body d-flex align-items-center gap-3">
+              <img
+                src="../assets/images/specialized/chip 2.png"
+                width="52px"
+                height="52px"
+                alt="frontend"
+              />
+              <h6 class="text-blue">AI and ML Engineers </h6>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-sm-6 col-lg-3 mt-3">
+          <div class="card card-specialized rounded-3">
+            <div class="card-body d-flex align-items-center gap-3">
+              <img
+                src="../assets/images/specialized/security 2.png"
+                width="52px"
+                height="52px"
+                alt="frontend"
+              />
+              <h6 class="text-blue">Application Security Engineers</h6>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-sm-6 col-lg-3 mt-3">
+          <div class="card card-specialized rounded-3">
+            <div class="card-body d-flex align-items-center gap-3">
+              <img
+                src="../assets/images/specialized/software-application 3.png"
+                width="52px"
+                height="52px"
+                alt="frontend"
+              />
+              <h6 class="text-blue">Fullstack Engineers</h6>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-sm-6 col-lg-3 mt-3">
+          <div class="card card-specialized rounded-3">
+            <div class="card-body d-flex align-items-center gap-3">
+              <img
+                src="../assets/images/specialized/technical-support 3.png"
+                width="52px"
+                height="52px"
+                alt="frontend"
+              />
+              <h6 class="text-blue">DevOps  Engineers</h6>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-sm-6 col-lg-3 mt-3">
+          <div class="card card-specialized rounded-3">
+            <div class="card-body d-flex align-items-center gap-3">
+              <img
+                src="../assets/images/specialized/data-science 2.png"
+                width="52px"
+                height="52px"
+                alt="frontend"
+              />
+              <h6 class="text-blue">Data Scientist / Data Engineers</h6>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-sm-6 col-lg-3 mt-3">
+          <div class="card card-specialized rounded-3">
+            <div class="card-body d-flex align-items-center gap-3">
+              <img
+                src="../assets/images/specialized/phone 2.png"
+                width="52px"
+                height="52px"
+                alt="frontend"
+              />
+              <h6 class="text-blue">IOS and Android Developers</h6>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-sm-6 col-lg-3 mt-3">
+          <div class="card card-specialized rounded-3">
+            <div class="card-body d-flex align-items-center gap-3">
+              <img
+                src="../assets/images/specialized/security 2.png"
+                width="52px"
+                height="52px"
+                alt="frontend"
+              />
+              <h6 class="text-blue">Application Security Engineers</h6>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-sm-6 col-lg-3 mt-3">
+          <div class="card card-specialized rounded-3">
+            <div class="card-body d-flex align-items-center gap-3">
+              <img
+                src="../assets/images/specialized/software-application 3.png"
+                width="52px"
+                height="52px"
+                alt="frontend"
+              />
+              <h6 class="text-blue">Fullstack Engineers</h6>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-sm-6 col-lg-3 mt-3">
+          <div class="card card-specialized rounded-3">
+            <div class="card-body d-flex align-items-center gap-3">
+              <img
+                src="../assets/images/specialized/technical-support 3.png"
+                width="52px"
+                height="52px"
+                alt="frontend"
+              />
+              <h6 class="text-blue">DevOps  Engineers</h6>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-sm-6 col-lg-3 mt-3">
+          <div class="card card-specialized rounded-3">
+            <div class="card-body d-flex align-items-center gap-3">
+              <img
+                src="../assets/images/specialized/data-science 2.png"
+                width="52px"
+                height="52px"
+                alt="frontend"
+              />
+              <h6 class="text-blue">Data Scientist / Data Engineers</h6>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-sm-6 col-lg-3 mt-3">
+          <div class="card card-specialized rounded-3">
+            <div class="card-body d-flex align-items-center gap-3">
+              <img
+                src="../assets/images/specialized/angular_solidBlack.png"
+                width="52px"
+                height="52px"
+                alt="frontend"
+              />
+              <h6 class="text-blue">Angular Developers</h6>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-sm-6 col-lg-3 mt-3">
+          <div class="card card-specialized rounded-3">
+            <div class="card-body d-flex align-items-center gap-3">
+              <img
+                src="../assets/images/specialized/chip 2.png"
+                width="52px"
+                height="52px"
+                alt="frontend"
+              />
+              <h6 class="text-blue">AI and ML Engineers </h6>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-sm-6 col-lg-3 mt-3">
+          <div class="card card-specialized rounded-3">
+            <div class="card-body d-flex align-items-center gap-3">
+              <img
+                src="../assets/images/specialized/security 2.png"
+                width="52px"
+                height="52px"
+                alt="frontend"
+              />
+              <h6 class="text-blue">Application Security Engineers</h6>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-sm-6 col-lg-3 mt-3">
+          <div class="card card-specialized rounded-3">
+            <div class="card-body d-flex align-items-center gap-3">
+              <img
+                src="../assets/images/specialized/software-application 3.png"
+                width="52px"
+                height="52px"
+                alt="frontend"
+              />
+              <h6 class="text-blue">Fullstack Engineers</h6>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-sm-6 col-lg-3 mt-3">
+          <div class="card card-specialized rounded-3">
+            <div class="card-body d-flex align-items-center gap-3">
+              <img
+                src="../assets/images/specialized/data-science 2.png"
+                width="52px"
+                height="52px"
+                alt="frontend"
+              />
+              <h6 class="text-blue">Data Scientist / Data Engineers</h6>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style >
+@import "@/src/assets/scss/components/_specialized.scss";
+</style>
--- src/pages/landing-page.vue
+++ src/pages/landing-page.vue
@@ -3,6 +3,7 @@
    <Header />
    <Process />
    <Expert />
+   <Specialize />
 </template>
 
 <script setup>
@@ -10,6 +11,7 @@ import Nav from "../components/nav.vue"
 import Header from "../components/header.vue"
 import Process from "../components/easy-process.vue"
 import Expert from "../components/expertise.vue"
+import Specialize from "../components/specialized.vue"
 
 
 
