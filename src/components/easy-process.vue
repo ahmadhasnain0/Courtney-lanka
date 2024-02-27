90a32dbfe05cf6bb075992bc47be7220a129c179
ahmadhasnain0 <ahmadhasnain4554@gmail.com>
2024-02-27 20:46:09 +0500

    easy process

---
+++ src/components/easy-process.vue
@@ -0,0 +1,16 @@
+<template>
+     <div class="container-fluid white pb-5 bg-gradient-primary">
+    <div class="container px-3 px-sm-0 w-100 align-items-center py-5">
+      <div class="row mt-4">
+<div class="col-12  text-center">
+    <p class="f-14 fw-bold text-white mb-2">How to Start</p>
+    <h2 class="fw-bold text-blue">Easy Process</h2>
+    <p class="f-12">
+        We specialize in helping you build a team of expert<br> developers, testers, and leaders.
+    </p>
+</div>
+
+      </div>
+      </div>
+      </div>
+</template>
--- src/pages/landing-page.vue
+++ src/pages/landing-page.vue
@@ -1,11 +1,15 @@
 <template>
    <Nav />
    <Header />
+   <Process />
 </template>
 
 <script setup>
 import Nav from "../components/nav.vue"
 import Header from "../components/header.vue"
+import Process from "../components/easy-process.vue"
+
+
 
 
 </script>
